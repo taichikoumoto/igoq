df6f52fdaa483f1eacfa09a20b45e9a2db1c6e92
taichikoumoto <sbskomoto@gmail.com>
2017-11-06 14:21:08 +0900

    fix

--- app/repositories/sheet_repository.rb
+++ app/repositories/sheet_repository.rb
@@ -2,9 +2,15 @@
 
 class SheetRepository
   class << self
+    def data
+      worksheet_session.rows
+    end
+
+    private
+
     def worksheet_session
       session = GoogleDrive::Session.from_service_account_key('igoq_service_account_key.json')
-      ws = session.spreadsheet_by_key('1SZdaF4kCmBqPO-ofIausOSCICJ3SEip8dTio_YHl87I').worksheets[0]
+      session.spreadsheet_by_key('1SZdaF4kCmBqPO-ofIausOSCICJ3SEip8dTio_YHl87I').worksheets[0]
 
       # レコード数を取得
       # p ws.num_rows
