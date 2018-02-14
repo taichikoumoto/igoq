22fb72a47c56b8debc80dfae543745d96744c83d
taichikoumoto <sbskomoto@gmail.com>
2018-02-14 17:07:07 +0900

    超過料金インポート修正

--- app/services/import_excess_service.rb
+++ app/services/import_excess_service.rb
@@ -4,12 +4,19 @@
 class ImportExcessService
   class << self
     def execute!(csv_file)
-      CSV.foreach(csv_file.path, headers: true, encoding: 'Shift_JIS:UTF-8') do |row|
-        number = row[2].delete('-')
+      header_to_sym_map = {
+        '電話番号' => :tel_number,
+        '通信料　メール（ＳＭＳ）（ＹＭ／他社宛）' => :sms_charge,
+        '通話料　通信サービス「０５７０等」' => :tel_charge
+      }
+      header_converter = lambda { |h| header_to_sym_map[h] }
+
+      CSV.foreach(csv_file.path, headers: true, header_converters: header_converter, encoding: 'Shift_JIS:UTF-8') do |row|
+        number = row[:tel_number].delete('-')
         phone = Phone.find_by(number: number)
         if phone
-          phone.excess_charge_sms = row[7].to_i
-          phone.excess_charge_tel = row[8].to_i
+          phone.excess_charge_sms = row[:sms_charge].to_i
+          phone.excess_charge_tel = row[:tel_charge].to_i
           phone.save!
         end
       end
